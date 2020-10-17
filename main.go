59024c2ae94b41d5e909c139517e88d762d1cb80
Wataru Ono <wtr0414@gmail.com>
2020-10-17 11:51:47 +0900

    :sparkles: DB から一覧を取得

--- main.go
+++ main.go
@@ -1,69 +1,91 @@
 package main
 
 import (
-  "encoding/json"
-  "fmt"
-  "log"
-  "net/http"
-  "strconv"
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
 
-  "github.com/gorilla/mux"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
 )
 
 type Todo struct {
-  ID int `json:"id"`
-  Content string `json:"content"`
-  Completed bool `json:"completed"`
+	ID 				int 		`json:"id"`
+	Content 	string 	`json:"content"`
+	Completed bool 		`json:"completed"`
 }
 
 var todos []Todo
 
 func getTodos(w http.ResponseWriter, r *http.Request) {
-  w.Header().Set("Content-Type", "application/json")
-  w.Header().Add("Access-Control-Allow-Origin", "*")
-  json.NewEncoder(w).Encode(todos)
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Add("Access-Control-Allow-Origin", "*")
+	_ = json.NewEncoder(w).Encode(todos)
 }
 
 func addTodo(w http.ResponseWriter, r *http.Request) {
-  var todo Todo
+	var todo Todo
 
-  json.NewDecoder(r.Body).Decode(&todo)
-  fmt.Println("todo: ", todo)
+	_ = json.NewDecoder(r.Body).Decode(&todo)
+	fmt.Println("todo: ", todo)
 
-  todos = append(todos, todo)
+	todos = append(todos, todo)
 
-  json.NewEncoder(w).Encode(todos)
+	_ = json.NewEncoder(w).Encode(todos)
 }
 
 func removeTodo(w http.ResponseWriter, r *http.Request) {
-  params := mux.Vars(r)
-  fmt.Println("params: ", params)
+	params := mux.Vars(r)
+	fmt.Println("params: ", params)
 
-  id, _ := strconv.Atoi(params["id"])
-  fmt.Println("id: ", id)
+	id, _ := strconv.Atoi(params["id"])
+	fmt.Println("id: ", id)
 
-  fmt.Println("todos: ", todos)
+	fmt.Println("todos: ", todos)
 
-  for i, item := range todos {
-    if item.ID == id {
-      todos = append(todos[:i], todos[i+1:]...)
-    }
-  }
-  json.NewEncoder(w).Encode(todos)
+	for i, item := range todos {
+		if item.ID == id {
+			todos = append(todos[:i], todos[i+1:]...)
+		}
+	}
+	_ = json.NewEncoder(w).Encode(todos)
 }
 
 func main() {
-  r := mux.NewRouter()
+	db, err := sql.Open("mysql", "root:includemath.h@tcp(127.0.0.1:3306)/react_go_todo")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
 
-  todos = append(todos,
-    Todo{ID: 1, Content: "砂糖を買う", Completed: false},
-    Todo{ID: 2, Content: "課題を終わらせる", Completed: false},
-    Todo{ID: 3, Content: "歯医者に行く", Completed: false},
-  )
+	rows, err := db.Query("select * from todo")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer rows.Close()
 
-  r.HandleFunc("/api/v1/todos", getTodos).Methods("GET")
-  r.HandleFunc("/api/v1/todos", addTodo).Methods("POST")
-  r.HandleFunc("/api/v1/todos/{id}", removeTodo).Methods("DELETE")
+	for rows.Next() {
+		var todo Todo
+		err := rows.Scan(&todo.ID, &todo.Content, &todo.Completed)
+		if err != nil {
+			panic(err.Error())
+		}
+		todos = append(todos, todo)
+	}
 
-  log.Fatal(http.ListenAndServe(":8080", r))
+	err = rows.Err()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	r := mux.NewRouter()
+
+	r.HandleFunc("/api/v1/todos", getTodos).Methods("GET")
+	r.HandleFunc("/api/v1/todos", addTodo).Methods("POST")
+	r.HandleFunc("/api/v1/todos/{id}", removeTodo).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
